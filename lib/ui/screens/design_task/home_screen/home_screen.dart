c6a0a6683bc4cccfc4f1dbde8752685fb0d214d7
AbdElRahman <abdelrahman-mohamed98@outlook.com>
2023-04-05 03:46:33 +0200

    separate widget

--- lib/ui/screens/design_task/home_screen/home_screen.dart
+++ lib/ui/screens/design_task/home_screen/home_screen.dart
@@ -1,5 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:dots_indicator/dots_indicator.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:super_sushi/ui/widgets/base_widgets/my_text_form_field.dart';
+import 'package:super_sushi/ui/widgets/design_task/home_widgets/slider_widget.dart';
 import 'package:super_sushi/utils/constants/magic_numbers.dart';
 import 'package:super_sushi/utils/extensions/asset_path.dart';
 import 'package:super_sushi/utils/extensions/empty_space.dart';
@@ -24,12 +23,6 @@ class _HomeScreenState extends State<HomeScreen> {
   Position? _position;
   String? _currentLocation;
   List<Placemark>? _placeMarks;
-  int indicatorIndex = 0;
-  List<String> _sliderImages = [
-    'slider'.toImage,
-    'slider'.toImage,
-    'slider'.toImage,
-  ];
 
   @override
   void initState() {
@@ -197,65 +190,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ],
                 ),
                 MagicNumbers.MARGIN_SIZE_LARGE.ph,
-                CarouselSlider(
-                  options: CarouselOptions(
-                    enlargeCenterPage: false,
-                    enableInfiniteScroll: true,
-                    height: MagicNumbers.sliderHeight,
-                    viewportFraction: 1,
-                    scrollDirection: Axis.horizontal,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 2),
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        indicatorIndex = index;
-                      });
-                    },
-                  ),
-                  items: _sliderImages
-                      .map(
-                        (image) => Container(
-                          height: MagicNumbers.sliderHeight,
-                          margin: const EdgeInsets.symmetric(
-                            horizontal: MagicNumbers.MARGIN_SIZE_EXTRA_SMALL,
-                          ),
-                          width: double.infinity,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(
-                              MagicNumbers.largeBorderRadius,
-                            ),
-                            child: Image.asset(
-                              image,
-                              height: MagicNumbers.sliderHeight,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ),
-                const SizedBox(height: 6),
-                DotsIndicator(
-                  dotsCount: _sliderImages.length,
-                  position: indicatorIndex.toDouble(),
-                  decorator: DotsDecorator(
-                    activeSize: const Size(20, 6),
-                    activeShape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        MagicNumbers.largeBorderRadius,
-                      ),
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        MagicNumbers.largeBorderRadius,
-                      ),
-                    ),
-                    size: const Size(20, 6),
-                    spacing: EdgeInsets.zero,
-                    color: Theme.of(context).backgroundColor,
-                    activeColor: Theme.of(context).primaryColor,
-                  ),
-                ),
+                const SliderWidget(),
               ],
             ),
           ),
---
+++ lib/ui/widgets/design_task/home_widgets/slider_widget.dart
@@ -0,0 +1,91 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:super_sushi/utils/constants/magic_numbers.dart';
+import 'package:super_sushi/utils/extensions/asset_path.dart';
+import 'package:super_sushi/utils/extensions/empty_space.dart';
+
+/// Slider of home screen
+class SliderWidget extends StatefulWidget {
+  /// No data needed
+  const SliderWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SliderWidget> createState() => _SliderWidgetState();
+}
+
+class _SliderWidgetState extends State<SliderWidget> {
+  int indicatorIndex = 0;
+  final List<String> _sliderImages = [
+    'slider'.toImage,
+    'slider'.toImage,
+    'slider'.toImage,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            enlargeCenterPage: false,
+            enableInfiniteScroll: true,
+            height: MagicNumbers.sliderHeight,
+            viewportFraction: 1,
+            scrollDirection: Axis.horizontal,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 2),
+            onPageChanged: (index, reason) {
+              setState(() {
+                indicatorIndex = index;
+              });
+            },
+          ),
+          items: _sliderImages
+              .map(
+                (image) => Container(
+                  height: MagicNumbers.sliderHeight,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: MagicNumbers.MARGIN_SIZE_EXTRA_SMALL,
+                  ),
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                      MagicNumbers.largeBorderRadius,
+                    ),
+                    child: Image.asset(
+                      image,
+                      height: MagicNumbers.sliderHeight,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+        MagicNumbers.PADDING_SIZE_SMALL.ph,
+        DotsIndicator(
+          dotsCount: _sliderImages.length,
+          position: indicatorIndex.toDouble(),
+          decorator: DotsDecorator(
+            activeSize: const Size(20, 6),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                MagicNumbers.largeBorderRadius,
+              ),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                MagicNumbers.largeBorderRadius,
+              ),
+            ),
+            size: const Size(20, 6),
+            spacing: EdgeInsets.zero,
+            color: Theme.of(context).backgroundColor,
+            activeColor: Theme.of(context).primaryColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
